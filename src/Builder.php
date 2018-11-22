e24a2710a956b6904b422fd467e3ca1be3c46785
git <DTxgpJd53rajPpXD>
2018-11-22 14:00:53 +0200

    fixes and addons

--- src/Builder.php
+++ src/Builder.php
@@ -93,6 +93,21 @@ class Builder
     }
     
     /**
+     * @param int $width Block width from left in points
+     * @param int $lines Max lines of text
+     * @param int    $line_spacing Space between the lines [-9999 - 9999]
+     * @param string $align Text justification [L(eft), C(enter), R(ight), J(ustified)]
+     *
+     * @return $this
+     */
+    public function block($width, $lines = 1, $line_spacing = 0, $align = 'L')
+    {
+        $this->zpl .= "^FB{$width},{$lines},{$line_spacing},{$align}";
+        
+        return $this;
+    }
+    
+    /**
      * @param string $code Barcode code
      * @param string $type Barcode type
      * @param int $height Barcode height
@@ -100,14 +115,20 @@ class Builder
      * @param string $orientation Orientation N(ormal)/R(otated 90)/I(inverted 180)/B(ottom up 270)
      * @param string $label Print label Y/N
      * @param string $label_above Positiion label above code Y/N
+     * @param int $pad_left Fixed code string length. Avoid to disable.
+     * @param string $pad_string Pad string
      *
      * @return $this
      */
-    public function barcode($code, $type, $height = 50, $line_width = 3, $orientation = 'N', $label = 'Y', $label_above = 'N')
+    public function barcode($code, $type, $height = 50, $line_width = 3, $orientation = 'N', $label = 'Y', $label_above = 'N', $pad_left = 0, $pad_string = '0')
     {
         $this->zpl .= "^BY{$line_width}^{$type}{$orientation},{$height},{$label},{$label_above}";
         
-        $this->text($code);
+        if ($pad_left) {
+            $code = str_pad($code, $pad_left, $pad_string, STR_PAD_LEFT);
+        }
+        
+        $this->text($code)->end();
         
         return $this;
     }
@@ -120,7 +141,7 @@ class Builder
     public function text($text)
     {
         $text = iconv('UTF-8', "ISO-8859-1//TRANSLIT", $text);
-        $this->zpl .= "^FD{$text}^FS";
+        $this->zpl .= "^FD{$text}";
         
         return $this;
     }
@@ -135,6 +156,32 @@ class Builder
     public function font($type = 'D', $height = null, $width = null)
     {
         $this->zpl .= "^A$type,$height,$width";
+        
+        return $this;
+    }
+    
+    /**
+     * @param int $width Square width
+     * @param int $height Square height (line count)
+     * @param int $line_thickness Square line thickness
+     *
+     * @return $this
+     */
+    public function square($width, $height, $line_thickness = 3)
+    {
+        $this->zpl .= "^GB{$width},{$height},$line_thickness";
+        
+        return $this;
+    }
+    
+    /**
+     * Denote the end of the field definition
+     *
+     * @return $this
+     */
+    public function end()
+    {
+        $this->zpl .= "^FS";
         
         return $this;
     }
