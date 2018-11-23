61ad7d6294ae796772346a84009dcbb8db456928
git <DTxgpJd53rajPpXD>
2018-11-23 10:09:46 +0200

    Refactor Client to accept optional port in host string

--- src/Client.php
+++ src/Client.php
@@ -12,14 +12,31 @@ class Client
     protected $socket;
     
     /**
+     * @var string
+     */
+    protected $host;
+    
+    /**
+     * @var int
+     */
+    protected $port = 9100;
+    
+    /**
      * Create an instance.
      *
-     * @param string $host
-     * @param int $port
+     * @param string $host Optionally with port [:9100]
      */
-    public function __construct($host, $port = 9100)
+    public function __construct(string $host)
     {
-        $this->connect($host, $port);
+        $exploded = explode(':', $host);
+        
+        $this->host = $exploded[0];
+        
+        if (isset($exploded[1])) {
+            $this->port = $exploded[1];
+        }
+        
+        $this->connect();
     }
     
     /**
@@ -34,26 +51,23 @@ class Client
      * Create an instance statically.
      *
      * @param string $host
-     * @param int $port
      * @return Client
      */
-    public static function printer(string $host, int $port = 9100): self
+    public static function printer(string $host): self
     {
-        return new static($host, $port);
+        return new static($host);
     }
     
     /**
      * Connect to printer.
      *
-     * @param string $host
-     * @param int $port
      * @throws CommunicationException if the connection fails.
      */
-    protected function connect(string $host, int $port): void
+    protected function connect(): void
     {
         $this->socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
         
-        if (!$this->socket || !@socket_connect($this->socket, $host, $port)) {
+        if (!$this->socket || !@socket_connect($this->socket, $this->host, $this->port)) {
             $error = $this->getLastError();
             throw new CommunicationException($error['message'], $error['code']);
         }
